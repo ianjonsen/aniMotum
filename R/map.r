fa68c3c95267c08f77c1d51408a7472fd43a57bc
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-02-27 11:32:54 -0400

    update map function imnports

--- R/map.r
+++ R/map.r
@@ -6,11 +6,11 @@
 ##' @param se include 2 * SE on time-series plots (SE's currently not displayed when proj = "ll")
 ##' @param outlier include outlier observations identified during prefilter-ing
 ##' @importFrom ggplot2 ggplot geom_sf geom_point geom_line geom_path aes ggtitle theme_bw theme element_blank
-##' @importFrom sf
-##' @importFrom ggspatial
+##' @importFrom sf st_bbox st_transform st_crop st_as_sf st_intersection
+##' @importFrom ggspatial layer_spatial annotation_spatial
 ##' @export
 
-plot.foieGras <- function(x, est = c("fitted","predicted"), se = TRUE, outlier = FALSE)
+map <- function(x, est = c("fitted","predicted"), se = TRUE, outlier = FALSE)
 {
   est <- match.arg(est)
   d <- if(!outlier) {
