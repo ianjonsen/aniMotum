433c006366a8ee8fc15b4d93ce73a355550bd6d9
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-02-27 13:03:57 -0400

    plotting coast w estimated locs but land polygons not properly segmented

--- R/map.r
+++ R/map.r
@@ -6,7 +6,7 @@
 ##' @param se include 2 * SE on time-series plots (SE's currently not displayed when proj = "ll")
 ##' @param outlier include outlier observations identified during prefilter-ing
 ##' @importFrom ggplot2 ggplot geom_sf geom_point geom_line geom_path aes ggtitle theme_bw theme element_blank
-##' @importFrom sf st_bbox st_transform st_crop st_as_sf st_intersection
+##' @importFrom sf st_bbox st_transform st_crop st_as_sf st_buffer st_crs
 ##' @importFrom ggspatial layer_spatial annotation_spatial
 ##' @export
 
@@ -28,26 +28,21 @@ map <- function(x, est = c("fitted","predicted"), se = TRUE, outlier = FALSE)
            sf_locs <- x$predicted
          })
 
-  prj <- st_crs(sf_locs)
+  orig_prj <- st_crs(sf_locs)
+
+  sf_locs_ll <- sf_locs %>% st_transform(4326)
+  bounds <- st_bbox(sf_locs_ll)
 
   ## get coastline shapes
-  bounds <- st_bbox(sf_locs)
   countriesLow <- NULL
   data("countriesLow", package = "rworldmap", envir = environment())
-
-  browser()
-
-  ## FIXME: this doesn't work - GEOS returning an evaluation error when trying to crop the transformed coast data
-  ## FIXME: try converting sf_locs to 4326 and then crop coast data to see if it's the polar projection that's
-  ## FIXME: causing the issue
   coast <- st_as_sf(countriesLow) %>%
-    st_transform(., prj) %>%
     st_crop(., bounds) %>%
-    st_buffer(0)
+    st_buffer(0) %>%
+    st_transform(., orig_prj)
 
-
-#  bb <- st_bbox(c(
-
+## FIXME: this code does not segment original countriesLow polygons properly
+## FIXME: not sure how to fix...
   browser()
 
 }
