71769db7b164d484740d1b262d70a6a6a70884a6
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-03-17 10:41:33 -0300

    add extract to deprecated fn list

--- R/foieGras-deprecated.R
+++ R/foieGras-deprecated.R
@@ -1,11 +1,17 @@
 #' Deprecated functions.
 #'
-#' \code{pluck} has been deprecated to avoid conflict with purrr::pluck. Instead use
+#' \code{extract} and \code{pluck} have been deprecated to avoid conflict with raster::extract, purrr::pluck. Instead use
 #' \code{grab}.
 #' @rdname foieGras-deprecated
 #' @param ... ignored
 #' @export
 #' @rdname foieGras-deprecated
 pluck <- function(...) {
+  .Deprecated("grab")
+}
+
+#' @export
+#' @rdname foieGras-deprecated
+extract <- function(...) {
   .Deprecated("grab")
 }
