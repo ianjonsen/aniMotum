a176d209b2ad91b0a8224d78425e9be0783d857e
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-03-17 10:42:59 -0300

    tweak

--- R/foieGras-deprecated.R
+++ R/foieGras-deprecated.R
@@ -1,6 +1,6 @@
 #' Deprecated functions.
 #'
-#' \code{extract} and \code{pluck} have been deprecated to avoid conflict with raster::extract, purrr::pluck. Instead use
+#' \code{extract} and \code{pluck} have been deprecated to avoid conflict with raster::extract, tidyr::extract, purrr::pluck. Instead use
 #' \code{grab}.
 #' @rdname foieGras-deprecated
 #' @param ... ignored
