bb4d655e8cca79c4b8ec2d2e96bf681b38513572
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-03-21 16:25:55 -0300

    dropped need for loading test model fit

--- tests/testthat/test-quickmap_ggplot.R
+++ tests/testthat/test-quickmap_ggplot.R
@@ -1,40 +1,38 @@
 context("test quickmap")
 
-data(testfit)
+data(ellie)
+fit <- fit_ssm(ellie, model="crw", time.step=24)
+
 ## step through the options incrementally
-p <- quickmap(testfit$ssm[[1]], what = "predicted")
+p <- quickmap(fit$ssm[[1]], what = "predicted")
 test_that("quickmap defaults return a ggplot object", {
   expect_s3_class(p, "ggplot")
 })
 
-p <- quickmap(testfit$ssm[[1]], what = "predicted", obs = TRUE)
+p <- quickmap(fit$ssm[[1]], what = "predicted", obs = TRUE)
 test_that("quickmap with obs plotted returns a ggplot object", {
   expect_s3_class(p, "ggplot")
 })
 
-p <- quickmap(testfit$ssm[[1]], what = "predicted", obs = TRUE, outlier = TRUE)
+p <- quickmap(fit$ssm[[1]], what = "predicted", obs = TRUE, outlier = TRUE)
 test_that("quickmap w all obs plotted returns a ggplot object", {
   expect_s3_class(p, "ggplot")
 })
 
-p <- quickmap(testfit$ssm[[1]], what = "predicted", obs = TRUE, outlier = TRUE, ext.rng=c(0.1, 0.1))
+p <- quickmap(fit$ssm[[1]], what = "predicted", obs = TRUE, outlier = TRUE, ext.rng=c(0.1, 0.1))
 test_that("quickmap with plot range extended by 10% in x,y returns a ggplot object", {
   expect_s3_class(p, "ggplot")
 })
 
-p <- quickmap(testfit$ssm[[1]], what = "predicted", obs = TRUE, outlier = TRUE, ext.rng=c(0.1, 0.1), size = 2.5)
+p <- quickmap(fit$ssm[[1]], what = "predicted", obs = TRUE, outlier = TRUE, ext.rng=c(0.1, 0.1), size = 2.5)
 test_that("quickmap with re-sized plot symbols returns a ggplot object", {
   expect_s3_class(p, "ggplot")
 })
 
-p <- quickmap(testfit$ssm[[1]], what = "predicted", obs = TRUE, outlier = TRUE, ext.rng=c(0.1, 0.1), size = 2.5, crs = "+init=epsg:3031 +lon_0=85")
+p <- quickmap(fit$ssm[[1]], what = "predicted", obs = TRUE, outlier = TRUE, ext.rng=c(0.1, 0.1), size = 2.5, crs = "+init=epsg:3031 +lon_0=85")
 test_that("quickmap with user-specified crs returns a ggplot object", {
   expect_s3_class(p, "ggplot")
 })
 
-data(ellie)
-test_that("quickmap returns error when input is not a foieGras object or output from grab()", {
-  expect_error(quickmap(ellie))
-})
 
 
