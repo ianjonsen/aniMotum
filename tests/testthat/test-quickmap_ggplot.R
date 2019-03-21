e0dd52964bc7679809d5485cce684344176affa7
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-03-21 11:19:32 -0300

    6 quickmap tests

--- tests/testthat/test-quickmap_ggplot.R
+++ tests/testthat/test-quickmap_ggplot.R
@@ -26,3 +26,9 @@ p <- quickmap(testfit$ssm[[1]], what = "predicted", obs = TRUE, outlier = TRUE, 
 test_that("quickmap with re-sized plot symbols returns a ggplot object", {
   expect_s3_class(p, "ggplot")
 })
+
+p <- quickmap(testfit$ssm[[1]], what = "predicted", obs = TRUE, outlier = TRUE, ext.rng=c(0.1, 0.1), size = 2.5, crs = "+init=epsg:3031 +lon_0=85")
+test_that("quickmap with user-specified crs returns a ggplot object", {
+  expect_s3_class(p, "ggplot")
+})
+
