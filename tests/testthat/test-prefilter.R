87daf2dc2bd5974b07794dbf721c97ae0d85b53a
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-03-30 16:54:52 -0300

    add test for prefilter with sf object

---
+++ tests/testthat/test-prefilter.R
@@ -0,0 +1,11 @@
+context("test prefilter")
+
+data(ellie)
+ellie_sf <- sf::st_as_sf(ellie, coords = c("lon","lat"), crs = 4326)
+
+## test run prefilter on sf version of daata
+f <- prefilter(ellie_sf, vmax=10, ang=-1, mind.dt=120)
+test_that("prefilter handles incoming sf data", {
+  expect_s3_class(f, "sf")
+  expect_equal(names(f), c("id","date","lc","smaj","smin","eor","keep","obs.type","amf_x","amf_y","geometry"))
+})
