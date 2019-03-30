0849de22696d27f11983997a279a4dd7880264f5
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-03-30 17:55:31 -0300

    more prefilter tests

--- tests/testthat/test-prefilter.R
+++ tests/testthat/test-prefilter.R
@@ -9,3 +9,27 @@ test_that("prefilter handles incoming sf data", {
   expect_s3_class(f, "sf")
   expect_equal(names(f), c("id","date","lc","smaj","smin","eor","keep","obs.type","amf_x","amf_y","geometry"))
 })
+
+## test on sf data with epsg 3031
+ellie_sf <- sf::st_transform(ellie_sf, crs = 3031)
+f <- prefilter(ellie_sf, vmax=10, ang=-1, min.dt=120)
+test_that("prefilter handles incoming sf data", {
+  expect_s3_class(f, "sf")
+  expect_equal(names(f), c("id","date","lc","smaj","smin","eor","keep","obs.type","amf_x","amf_y","geometry"))
+})
+
+## test that prefilter converts sf w units=m to units=km
+ellie_sf <- sf::st_transform(ellie_sf, crs = "+init=epsg:3031 + units=m")
+f <- prefilter(ellie_sf, vmax=10, ang=-1, min.dt=120)
+test_that("prefilter handles incoming sf data", {
+  expect_s3_class(f, "sf")
+  expect_equal(names(f), c("id","date","lc","smaj","smin","eor","keep","obs.type","amf_x","amf_y","geometry"))
+})
+
+## test prefilter catches data that does not have 5 or 8 columns
+test_that("prefilter handles incoming sf data", {
+  expect_error(prefilter(ellie_sf[, -5], vmax=10, ang=-1, min.dt=120))
+}
+
+
+)
