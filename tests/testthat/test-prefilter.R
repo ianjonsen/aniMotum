318e9efcb97dde9bba9af1a858bfa46efa812734
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-07-03 13:12:59 -0300

    fix to accomodate new sf

--- tests/testthat/test-prefilter.R
+++ tests/testthat/test-prefilter.R
@@ -5,6 +5,7 @@ ellie_sf <- sf::st_as_sf(ellie, coords = c("lon","lat"), crs = 4326)
 
 ## test run prefilter on sf version of daata
 f <- prefilter(ellie_sf, vmax=10, ang=-1, min.dt=120)
+f <- dplyr::select(f, id, date, lc, smaj, smin,eor,keep,obs.type,amf_x,amf_y,geometry)
 test_that("prefilter handles incoming sf data", {
   expect_s3_class(f, "sf")
   expect_equal(names(f), c("id","date","lc","smaj","smin","eor","keep","obs.type","amf_x","amf_y","geometry"))
@@ -13,6 +14,7 @@ test_that("prefilter handles incoming sf data", {
 ## test on sf data with epsg 3031
 ellie_sf <- sf::st_transform(ellie_sf, crs = 3031)
 f <- prefilter(ellie_sf, vmax=10, ang=-1, min.dt=120)
+f <- dplyr::select(f, id, date, lc, smaj, smin,eor,keep,obs.type,amf_x,amf_y,geometry)
 test_that("prefilter handles incoming sf data", {
   expect_s3_class(f, "sf")
   expect_equal(names(f), c("id","date","lc","smaj","smin","eor","keep","obs.type","amf_x","amf_y","geometry"))
@@ -21,6 +23,7 @@ test_that("prefilter handles incoming sf data", {
 ## test that prefilter converts sf w units=m to units=km
 ellie_sf <- sf::st_transform(ellie_sf, crs = "+init=epsg:3031 + units=m")
 f <- prefilter(ellie_sf, vmax=10, ang=-1, min.dt=120)
+f <- dplyr::select(f, id, date, lc, smaj, smin,eor,keep,obs.type,amf_x,amf_y,geometry)
 test_that("prefilter handles incoming sf data", {
   expect_s3_class(f, "sf")
   expect_equal(names(f), c("id","date","lc","smaj","smin","eor","keep","obs.type","amf_x","amf_y","geometry"))
