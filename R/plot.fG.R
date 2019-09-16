49be8c6cd0e4f674a4bca2a1b87f72cfb167ed93
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-09-16 10:19:12 -1000

    plot's for multiple fits via fG tibble

--- R/plot.fG.R
+++ R/plot.fG.R
@@ -4,22 +4,26 @@
 ##'
 ##' @param x a fG compound tibble
 ##' @param what specify which location estimates to display on time-series plots: fitted or predicted
+##' @param type of plot to generate: 1-d time series for lon and lat separately (type = 1, default) or 2-d track plot (type = 2)
+##' @param lc display Argos location classes
 ##' @param outlier include all extreme outliers flagged by prefilter in plots (logical)
 ##' @param ... additional arguments to be ignored
-##' @importFrom ggplot2 ggplot geom_point geom_path aes_string ggtitle theme_bw theme element_blank
-##' @importFrom ggplot2 element_text xlab
-##' @importFrom gridExtra grid.arrange
-##' @importFrom magrittr "%>%"
+##' @importFrom ggplot2 ggplot geom_point geom_path aes_string ggtitle theme_bw theme element_blank geom_rug geom_path
+##' @importFrom ggplot2 element_text xlab scale_colour_brewer theme_dark labeller label_both label_value
+##' @importFrom tidyr gather
+##' @importFrom dplyr "%>%" select bind_cols rename filter
+##' @importFrom tibble as_tibble
 ##' @method plot fG
 ##'
 ##' @examples
 ##' ## load example foieGras fit object (to save time)
 ##' data(fit)
-##' plot(fit$ssm[[1]])
+##' plot(fit, what = "f", type = 1)
+##' plot(fit, what = "p", type = 2, lc = TRUE)
 ##'
 ##' @export
 
-plot.fG <- function(x, what = c("fitted","predicted"), outlier = FALSE, ...)
+plot.fG <- function(x, what = c("fitted","predicted"), type = 1, lc = FALSE, outlier = FALSE, ...)
 {
 
   if (length(list(...)) > 0) {
@@ -29,11 +33,70 @@ plot.fG <- function(x, what = c("fitted","predicted"), outlier = FALSE, ...)
   what <- match.arg(what)
 
   if(inherits(x, "fG")) {
-    f <- grab(x, "fitted", as_sf = FALSE)
-    p <- grab(x, "predicted", as_sf = FALSE)
-    d <- grab(x, "data", as_sf = FALSE)
+    switch(what,
+           fitted = {
+             ssm <- grab(x, "fitted", as_sf = FALSE)
+           },
+           predicted = {
+             ssm <- grab(x, "predicted", as_sf = FALSE)
+           })
 
-    browser()
+    d <- grab(x, "data", as_sf = FALSE) %>%
+      mutate(lc = factor(lc, levels=c("3","2","1","0","A","B","Z"), ordered=TRUE))
+    if(!outlier) d <- d %>% filter(keep)
+
+    if(type == 1) {
+    foo <- ssm %>% select(id, lon, lat) %>% gather(., key = "coord", value = "value", lon, lat)
+    bar <- rep(ssm$date, 2) %>% as_tibble() %>% rename(date = "value")
+
+    foo.d <- d %>% select(id, lon, lat) %>% gather(., key = "coord", value = "value", lon, lat)
+    bar.d <- d %>% select(date, lc) %>% bind_rows(., .)
+
+    pd <- bind_cols(foo, bar) %>%
+      select(id, date, coord, value)
+    dd <- bind_cols(foo.d, bar.d) %>%
+      select(id, date, lc, coord, value)
+
+    p <- ggplot(pd, aes(date, value))
+
+    if(lc) {
+      p <- p + geom_point(col="firebrick", size = 0.6) +
+        geom_point(data = dd, aes(date, value, colour = lc), alpha = 0.5, size = 1.25)
+    } else {
+      p <- p + geom_point(data = dd, aes(date, value), colour = "dodgerblue",
+                          alpha = 0.7, size = 1.25) +
+        geom_point(col="firebrick", size = 0.6)
+    }
+      p <- p + geom_rug(data = dd, aes(date), col = "dodgerblue", alpha=0.75, sides = "b") +
+        facet_wrap(id ~ coord, scales = "free", ncol=2,
+                   labeller = labeller(id = label_both, coord = label_value))
+
+    if(lc) {
+        p <- p + scale_colour_brewer(type = "seq", palette = "YlGnBu") +
+          theme_dark()
+      }
+
+    } else if (type == 2) {
+
+    p <- ggplot() +
+      geom_path(data = ssm, aes(lon, lat), col = grey(0.7), alpha = 0.5, lwd = 0.5) +
+      geom_point(data = ssm, aes(lon, lat), col = grey(0.7), alpha = 0.5, size = 0.9)
+
+      if(lc) {
+        p <- p + geom_point(data = d, aes(lon, lat, colour = lc), size = 1.25)
+      } else {
+        p <- p + geom_point(data = d, aes(lon, lat), colour = "dodgerblue",
+                            size = 1.25, alpha = 0.7)
+      }
+    p <- p + facet_wrap( ~ id, scales = "free", ncol = 2,
+                         labeller = labeller(id = label_both))
+
+    if(lc) {
+      p <- p + scale_colour_brewer(type = "seq", palette = "YlGnBu") +
+      theme_dark()
+    }
+    }
+  print(p)
 
   } else {
     stop("x must be an `fG` compound tibble")
