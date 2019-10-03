9d2bdc010c33f94c71541fd8fa7a2b8e57f841d5
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-10-03 12:31:43 -0300

    remove boxplot from plot.osar

--- R/plot.fG_osar.R
+++ R/plot.fG_osar.R
@@ -3,8 +3,8 @@
 ##' @description plot One-Step-Ahead (prediction) residuals from a \code{foieGras osar} object
 ##'
 ##' @param x a \code{foieGras osar} object with class `fG_osar`
-##' @param type type of residual plot to generate; either qqnorm (default), histogram or boxplot
-##' @param bw binwidth for histogram plots (see ggplot2::geom_histogram for details)
+##' @param type type of residual plot to generate; either qqnorm (default) or histogram
+##' @param bw binwidth for histogram plots (see ggplot2::geom_histogram for details), ignored if type = "qqnorm"
 ##' @param ... additional arguments to be ignored
 ##' @importFrom ggplot2 ggplot geom_qq geom_qq_line geom_histogram geom_boxplot geom_vline geom_hline
 ##' @importFrom ggplot2 aes facet_grid facet_wrap coord_flip
@@ -20,7 +20,7 @@
 ##'
 ##' @export
 
-plot.fG_osar <- function(x, type = c("qqnorm", "histogram", "boxplot"), bw = 0.5, ...)
+plot.fG_osar <- function(x, type = c("qqnorm", "histogram"), bw = 0.5, ...)
 {
   if (length(list(...)) > 0) {
     warning("additional arguments ignored")
@@ -42,13 +42,6 @@ plot.fG_osar <- function(x, type = c("qqnorm", "histogram", "boxplot"), bw = 0.5
              geom_histogram(binwidth = bw, col = grey(0.9), lwd = 0.5) +
              geom_vline(xintercept = 0, lty = 2, col = "firebrick") +
              facet_grid(id ~ coord)
-         },
-         boxplot = {
-           p <- ggplot(x %>% filter(!is.na(resid)), aes(x = id, y = resid)) +
-             geom_boxplot() +
-             geom_hline(yintercept = 0, lty = 2, col = "firebrick") +
-             coord_flip() +
-             facet_wrap( ~ coord)
          })
 
   print(p)
