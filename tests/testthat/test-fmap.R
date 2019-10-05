93cbb6b920b6f02d739f93f7c07318d9d1d1e618
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-10-05 12:01:06 -0300

    gen fixes

--- tests/testthat/test-fmap.R
+++ tests/testthat/test-fmap.R
@@ -18,7 +18,8 @@ test_that("mp with obs plotted has s3 classes `gg`, `ggplot`", {
   expect_s3_class(mp, c("gg","ggplot"))
 })
 
-mp <- fmap(fssm, what = "predicted", crs = "+init=epsg:3031 +lon_0=85")
+mp <- fmap(fssm, what = "predicted", 
+           crs = "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=85 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=km +no_defs")
 test_that("mp with new crs has s3 classes `gg`, `ggplot`", {
   expect_s3_class(mp, c("gg","ggplot"))
 })
@@ -33,7 +34,9 @@ test_that("mp with resized points has s3 classes `gg`, `ggplot`", {
   expect_s3_class(mp, c("gg","ggplot"))
 })
 
-mp <- fmap(fssm, what = "predicted", obs = TRUE, crs="+init=epsg:3031 +lon_0=85", ext.rng=c(0.025, 0.075), size = 1.5)
+mp <- fmap(fssm, what = "predicted", obs = TRUE, 
+           crs="+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=85 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=km +no_defs", 
+           ext.rng=c(0.025, 0.075), size = 1.5)
 test_that("mp with all arg's exercised has s3 classes `gg`, `ggplot`", {
   expect_s3_class(mp, c("gg","ggplot"))
 })
--- tests/testthat/test-prefilter.R
+++ tests/testthat/test-prefilter.R
@@ -1,7 +1,7 @@
 context("test prefilter")
 
 data(ellie)
-ellie_sf <- sf::st_as_sf(ellie, coords = c("lon","lat"), crs = 4326)
+ellie_sf <- sf::st_as_sf(ellie, coords = c("lon","lat"), crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
 
 ## test run prefilter on sf version of daata
 f <- prefilter(ellie_sf, vmax=10, ang=-1, min.dt=120)
@@ -12,7 +12,8 @@ test_that("prefilter handles incoming sf data", {
 })
 
 ## test on sf data with epsg 3031
-ellie_sf <- sf::st_transform(ellie_sf, crs = 3031)
+ellie_sf <- sf::st_transform(ellie_sf, 
+                             crs = "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=km +no_defs")
 f <- prefilter(ellie_sf, vmax=10, ang=-1, min.dt=120)
 f <- dplyr::select(f, id, date, lc, smaj, smin,eor,keep,obs.type,emf.x,emf.y,geometry)
 test_that("prefilter handles incoming sf data", {
@@ -21,7 +22,8 @@ test_that("prefilter handles incoming sf data", {
 })
 
 ## test that prefilter converts sf w units=m to units=km
-ellie_sf <- sf::st_transform(ellie_sf, crs = "+init=epsg:3031 +units=m")
+ellie_sf <- sf::st_transform(ellie_sf, 
+                             crs = "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
 f <- prefilter(ellie_sf, vmax=10, ang=-1, min.dt=120)
 f <- dplyr::select(f, id, date, lc, smaj, smin,eor,keep,obs.type,emf.x,emf.y,geometry)
 test_that("prefilter handles incoming sf data", {
