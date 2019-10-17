b087c98e5842871854f1d33141ef54245f4d28cf
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-10-17 14:55:57 -0300

    fix misplaced bracket on obs.type "GL"

--- R/sfilter.R
+++ R/sfilter.R
@@ -186,7 +186,7 @@ sfilter <-
                            mu = factor(rbind(rep(NA, nrow(xs)), rep(NA, nrow(xs)))),
                            v =  factor(rbind(rep(NA, nrow(xs)), rep(NA, nrow(xs))))
                            )
-                    } else if(pls == 0 && unique(d$obs.type == "GL")) {
+                    } else if(pls == 0 && unique(d$obs.type) == "GL") {
                       list(l_tau = factor(c(NA, NA)),
                            l_psi = factor(NA),
                            logD = factor(NA),
