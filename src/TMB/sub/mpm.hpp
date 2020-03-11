01fe2858cd22b6cfb251822b26f341c23a05944e
Ian Jonsen <ian.jonsen@mq.edu.au>
2020-03-10 21:46:49 -0300

    add ability to handle temporally irregular locations

--- src/TMB/sub/mpm.hpp
+++ src/TMB/sub/mpm.hpp
@@ -11,9 +11,11 @@ Type mpm(objective_function<Type>* obj) {
   
   DATA_MATRIX(x);                   // locations
   DATA_INTEGER(N);                  // number of time.steps to iterate over
+  DATA_VECTOR(dt);                  // dt is time interval between x_i and x_{i-1}
   PARAMETER_VECTOR(lg);		          // Autocorrelation parameter (link scale)
   PARAMETER_VECTOR(l_sigma);	      // Innovation variance (log scale)
   PARAMETER(l_sigma_g);             // logistic scale parameter of rw on lg (log scale)
+  
   
   // Backtransform parameters from link scale
   vector<Type> gamma = Type(1.0) / (Type(1.0) + exp(-lg));
@@ -22,15 +24,13 @@ Type mpm(objective_function<Type>* obj) {
   
   // 2x2 covariance matrix for innovations
   matrix<Type> cov(2,2);
-  cov(0,0) = sigma(0) * sigma(0);
-  cov(0,1) = 0.0;
-  cov(1,0) = 0.0;
-  cov(1,1) = sigma(1) * sigma(1);
+//  cov(0,0) = sigma(0) * sigma(0);
+//  cov(0,1) = 0.0;
+//  cov(1,0) = 0.0;
+//  cov(1,1) = sigma(1) * sigma(1);
   
   Type jnll = 0.0;
   vector<Type> mu(2);
-  
-  MVNORM_t<Type> nll_dens(cov);   // Multivariate Normal density
   int j;
   
     for(j = 1; j < N; ++j) {
@@ -38,7 +38,14 @@ Type mpm(objective_function<Type>* obj) {
     }
     
     for(j = 2; j < N; ++j){
-      mu = x.row(j) - x.row(j-1) - gamma(j-1) * (x.row(j-1) - x.row(j-2));  // first diff RW on locations
+      // var-cov depends on time interval
+      cov.setZero();
+      cov(0,0) = sigma(0) * sigma(0) * dt(j) * dt(j);
+      cov(1,1) = sigma(1) * sigma(1) * dt(j) * dt(j);
+      // first diff RW on locations
+      mu = x.row(j) - x.row(j-1) - gamma(j-1) * (dt(j)/dt(j-1)) * (x.row(j-1) - x.row(j-2));  
+      
+      MVNORM_t<Type> nll_dens(cov);   // Multivariate Normal density
       jnll += nll_dens(mu);
     }
   
