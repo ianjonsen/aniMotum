a11f184c5fe946a3483341b36e60282d7eb53d6f
Ian Jonsen <ian.jonsen@mq.edu.au>
2020-06-25 09:48:34 -0300

    add outlier control, wesanderson palette

--- R/plot.fG_ssm.R
+++ R/plot.fG_ssm.R
@@ -5,6 +5,7 @@
 ##' @param x a \code{foieGras} ssm fit object with class `fG_ssm`
 ##' @param what specify which location estimates to display on time-series plots: fitted or predicted
 ##' @param type of plot to generate: 1-d time series for lon and lat separately (type = 1, default) or 2-d track plot (type = 2)
+##' @param outlier include outlier locations dropped by prefilter (outlier = TRUE, default)
 ##' @param ncol number of columns to use for faceting. Default is ncol = 1 but this may be increased for multi-individual fit objects
 ##' @param ... additional arguments to be ignored
 ##' 
@@ -13,10 +14,11 @@
 ##' or (type = 2) 2-d fits to data (units = km)
 ##' 
 ##' @importFrom ggplot2 ggplot geom_point geom_path aes_string ggtitle geom_rug
-##' @importFrom ggplot2 element_text xlab scale_colour_brewer labeller label_both label_value geom_ribbon
+##' @importFrom ggplot2 element_text xlab labeller label_both label_value geom_ribbon
 ##' @importFrom tidyr gather
-##' @importFrom dplyr "%>%" select bind_cols rename
+##' @importFrom dplyr "%>%" select bind_cols rename filter
 ##' @importFrom tibble enframe
+##' @importFrom wesanderson wes_palette
 ##' @method plot fG_ssm
 ##'
 ##' @examples
@@ -27,13 +29,15 @@
 ##'
 ##' @export
 
-plot.fG_ssm <- function(x, what = c("fitted","predicted"), type = 1, ncol = 1, ...)
+plot.fG_ssm <- function(x, what = c("fitted","predicted"), type = 1, outlier = TRUE, ncol = 1, ...)
 {
   if (length(list(...)) > 0) {
     warning("additional arguments ignored")
   }
   
   what <- match.arg(what)
+  
+  wpal <- wes_palette("Darjeeling2", n = 5, "discrete")
   
   if(inherits(x, "fG_ssm")) {
     switch(what,
@@ -42,14 +46,22 @@ plot.fG_ssm <- function(x, what = c("fitted","predicted"), type = 1, ncol = 1, .
            },
            predicted = {
              if(any(sapply(x$ssm, function(.) is.na(.$ts)))) {
-               stop("\n there are no predicted locations because you used time.step = NA when calling `fit_ssm`")
+               ssm <- grab(x, "fitted", as_sf = FALSE)
+               warning("there are no predicted locations because you used time.step = NA when calling `fit_ssm`, 
+                       plotting fitted locations instead")
              } else {
              ssm <- grab(x, "predicted", as_sf = FALSE)
              }
            })
     
-    d <- grab(x, "data", as_sf = FALSE) %>%
-      mutate(lc = factor(lc, levels=c("3","2","1","0","A","B","Z"), ordered=TRUE))
+    if(outlier) {
+      d <- grab(x, "data", as_sf = FALSE) %>%
+        mutate(lc = factor(lc, levels=c("3","2","1","0","A","B","Z"), ordered=TRUE))
+    } else {
+      d <- grab(x, "data", as_sf = FALSE) %>%
+        mutate(lc = factor(lc, levels=c("3","2","1","0","A","B","Z"), ordered=TRUE)) %>%
+        filter(keep)
+    }
     
     if(type == 1) {
       
@@ -58,34 +70,53 @@ plot.fG_ssm <- function(x, what = c("fitted","predicted"), type = 1, ncol = 1, .
       bar <- rep(ssm$date, 2) %>% enframe(name = NULL) %>% rename(date = "value")
       
       foo.d <- d %>% select(id, x, y) %>% gather(., key = "coord", value = "value", x, y)
-      bar.d <- d %>% select(date, lc) %>% bind_rows(., .)
+      bar.d <- d %>% select(date, lc, keep) %>% bind_rows(., .)
       
       pd <- bind_cols(foo, foo.se, bar) %>% select(id, date, coord, value, se)
-      dd <- bind_cols(foo.d, bar.d) %>% select(id, date, lc, coord, value)
-         
-      p <- ggplot() + 
-        geom_point(data = dd, aes(date, value), colour = "dodgerblue",
-                            alpha = 0.7, size = 1.25) + 
-        geom_ribbon(data = pd, aes(date, ymin = value - 2 * se, ymax = value + 2 * se), fill="firebrick", alpha = 0.25) + 
-        geom_point(data = pd, aes(date, value), col="firebrick", size = 0.6) + 
-        geom_rug(data = dd, aes(date), col = "dodgerblue", alpha=0.75, sides = "b") + 
-        facet_wrap(id ~ coord, scales = "free", ncol = ncol,
+      dd <- bind_cols(foo.d, bar.d) %>% select(id, date, lc, coord, value, keep)
+      
+      if(outlier) {
+        p <- ggplot() + 
+          geom_point(data = dd %>% filter(!keep), aes(date, value), 
+                     size = 1, colour = wpal[5], alpha = 0.5, shape = 4) +
+          geom_point(data = dd %>% filter(keep), aes(date, value), 
+                     size = 1.25, colour = wpal[3], shape = 19, alpha = 0.8) +
+          geom_rug(data = dd %>% filter(!keep), aes(date), colour = wpal[5], alpha = 0.5, sides = "b") + 
+          geom_rug(data = dd %>% filter(keep), aes(date), colour = wpal[3], alpha=0.7, sides = "b")
+      } else {
+        p <- ggplot() + 
+          geom_point(data = dd %>% filter(keep), aes(date, value), 
+                     size = 1.25, colour = wpal[3], shape = 19, alpha = 0.8) +
+          geom_rug(data = dd %>% filter(keep), aes(date), colour = wpal[3], alpha=0.7, sides = "b")
+      }  
+       p <- p + 
+         geom_ribbon(data = pd, aes(date, ymin = value - 2 * se, ymax = value + 2 * se), fill=wpal[2], alpha = 0.25) + 
+         geom_point(data = pd, aes(date, value), col=wpal[2], size = 0.6) + 
+         facet_wrap(id ~ coord, scales = "free", ncol = ncol,
                    labeller = labeller(id = label_both, coord = label_value))
-
+        
       
     } else if (type == 2) {
-      
-      p <- ggplot() + geom_point(data = d, aes(x, y), colour = "dodgerblue",
-                              size = 1.25, alpha = 0.7)
-      
-      p <- p + geom_path(data = ssm, aes(x, y), col = "firebrick", alpha = 0.5, lwd = 0.25) +
-        geom_point(data = ssm, aes(x, y), col = "firebrick", alpha = 0.5, size = 0.6) + 
-        facet_wrap( ~ id, scales = "free", ncol = ncol, labeller = labeller(id = label_both))
+      if(outlier) {
+      p <- ggplot() + 
+        geom_point(data = d %>% filter(!keep), aes(x, y), 
+                   size = 1, colour = wpal[5], alpha = 0.5, shape = 4) +
+        geom_point(data = d %>% filter(keep), aes(x, y), 
+                              size = 1.25, colour = wpal[3], shape = 19, alpha = 0.8)
+      } else {
+        p <- ggplot() + 
+          geom_point(data = d %>% filter(keep), aes(x, y), 
+                     size = 1.25, colour = wpal[3], shape = 19, alpha = 0.8)
+      }
+        p <- p + 
+          geom_path(data = ssm, aes(x, y), col = wpal[2], lwd = 0.2, alpha = 0.4) +
+          geom_point(data = ssm, aes(x, y), col = wpal[2], size = 0.6) + 
+          facet_wrap( ~ id, scales = "free", ncol = ncol, labeller = labeller(id = label_both))
       
     }
     return(p)
     
   } else {
-    stop("x must be a fG_ssm compound tibble")
+    stop("x must be a fG_ssm tibble")
   }
 }
