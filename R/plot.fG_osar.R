c6486123cf5bd7ac4222a27e6c5b405adfecc1b1
Ian Jonsen <ian.jonsen@mq.edu.au>
2021-03-17 12:46:16 -0300

    making plot.fG_osar more flexible

--- R/plot.fG_osar.R
+++ R/plot.fG_osar.R
@@ -3,11 +3,12 @@
 ##' @description plot One-Step-Ahead (prediction) residuals from a \code{foieGras osar} object
 ##'
 ##' @param x a \code{foieGras osar} object with class `fG_osar`
-##' @param type type of residual plot to generate; either qqnorm (default) or histogram
+##' @param type type of residual plot to generate; time-series (ts), qqnorm (default, qq) or acf
 ##' @param bw binwidth for histogram plots (see ggplot2::geom_histogram for details), ignored if type = "qqnorm"
 ##' @param ... additional arguments to be ignored
-##' @importFrom ggplot2 ggplot geom_qq geom_qq_line geom_histogram geom_boxplot geom_vline geom_hline
+##' @importFrom ggplot2 ggplot geom_qq geom_qq_line geom_segment geom_boxplot geom_hline
 ##' @importFrom ggplot2 aes facet_grid facet_wrap coord_flip
+##' @importFrom stats acf qnorm
 ##' @importFrom wesanderson wes_palette
 ##' @method plot fG_osar
 ##'
@@ -19,7 +20,7 @@
 ##'
 ##' @export
 
-plot.fG_osar <- function(x, type = c("qqnorm", "histogram"), bw = 0.5, ...)
+plot.fG_osar <- function(x, type = c("ts", "qqnorm", "acf"), bw = 0.5, ...)
 {
   if (length(list(...)) > 0) {
     warning("additional arguments ignored")
@@ -31,6 +32,12 @@ plot.fG_osar <- function(x, type = c("qqnorm", "histogram"), bw = 0.5, ...)
   if(inherits(x, "fG_osar")) {
   
   switch(type,
+         ts = {
+           p <- ggplot(x) +
+             geom_point(aes(x = 1:length(residual), y = residual), shape = 19, colour = wpal[1]) +
+             geom_hline(aes(yintercept = 0), lty = 2, colour = wpal[4]) +
+             facet_grid(id ~ coord, scales = "free_x")
+         }, 
          qqnorm = {
            x <- x[!is.na(x$residual), ]
            p <- ggplot(x, aes(sample = residual)) +
@@ -38,12 +45,24 @@ plot.fG_osar <- function(x, type = c("qqnorm", "histogram"), bw = 0.5, ...)
              geom_qq_line(colour = wpal[4]) +
              facet_grid(id ~ coord)
          },
-         histogram = {
+         acf = {
+           browser()
            x <- x[!is.na(x$residual), ]
-           p <- ggplot(x, aes(x = residual)) +
-             geom_histogram(binwidth = bw, col = grey(0.9), lwd = 0.5, fill = wpal[1]) +
-             geom_vline(xintercept = 0, colour = wpal[4]) +
-             facet_grid(id ~ coord)
+           x.acf <- acf(x[x$coord == "x", "residual"], plot = FALSE)
+           y.acf <- acf(x[x$coord == "y", "residual"], plot = FALSE)
+           x1 <- rbind(with(x.acf, data.frame(lag, acf, coord = rep("x", length(x.acf$lag)))), 
+                       with(y.acf, data.frame(lag, acf, coord = rep("y", length(y.acf$lag))))
+                       )
+           cil.x <- qnorm((1 - 0.95)/2) / sqrt(nrow(x[x$coord == "x", ]))
+           cil.y <- qnorm((1 - 0.95)/2) / sqrt(nrow(x[x$coord == "y", ]))
+           cil <- data.frame(ci = c(cil.x,cil.y), coord = c("x","y"))
+           
+           p <- ggplot(x1, aes(x = lag, y = acf)) +
+             geom_hline(aes(yintercept = 0), colour = wpal[4]) +
+             geom_segment(aes(xend = lag, yend = 0), colour = wpal[1]) +
+             geom_hline(data = cil, aes(yintercept = ci), linetype = 2, color = wpal[2]) +
+             geom_hline(data = cil, aes(yintercept = -ci), linetype = 2, color = wpal[2]) +
+             facet_grid( ~ coord)
          })
 
   print(p)
