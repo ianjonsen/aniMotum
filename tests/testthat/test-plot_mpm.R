b55a2e06ffba9c6353af24b6a5268c800d0feedb
Ian Jonsen <ian.jonsen@mq.edu.au>
2022-05-18 11:24:49 -0300

    update README install instructions; fix test-plot_mpm

--- tests/testthat/test-plot_mpm.R
+++ tests/testthat/test-plot_mpm.R
@@ -9,43 +9,43 @@ xs <- fit_ssm(ellie, spdf=FALSE, model = "rw", time.step=72,
 xm <- fit_mpm(xs, model = "jmpm")
 
 ## plot gamma time-series on a single page
-tp <- plot(xm, pages = 1)
 test_that("plot completes silently - fitted", {
+  tp <- plot(xm, pages = 1)
   expect_s3_class(tp, c("gg","ggplot"))
 })
 
 ## plot gamma time-series on separate pages for each individual
-tp <- plot(xm, pages = 0, ask = FALSE)
 test_that("plot completes silently - predicted", {
+  tp <- plot(xm, pages = 0, ask = FALSE)
   expect_type(tp, "list")
   expect_s3_class(tp[[1]], c("gg","ggplot"))
 })
 
 ## plot gamma-coloured locations along track
-tp <- plot(xm, xs, pages = 1)
 test_that("plot completes silently - predicted", {
+  tp <- plot(xm, xs, pages = 1)
   expect_s3_class(tp, c("gg","ggplot"))
 })
 
 ## plot gamma-coloured locations along track & scale size according to se
-tp <- plot(xm, xs, se = TRUE, pages = 1)
 test_that("plot completes silently - predicted", {
+  tp <- plot(xm, xs, se = TRUE, pages = 1)
   expect_s3_class(tp, c("gg","ggplot"))
 })
 
 ## plot gamma-coloured locations along track in 2 columns
-tp <- plot(xm, xs, pages = 1, ncol = 2)
 test_that("plot completes silently - predicted", {
+  tp <- plot(xm, xs, pages = 1, ncol = 2)
   expect_s3_class(tp, c("gg","ggplot"))
 })
 
-tp <- plot(xm, xs, pages = 0, ask = TRUE)
 test_that("plot completes silently - predicted", {
+  tp <- plot(xm, xs, pages = 0, ask = TRUE)
   expect_type(tp, "logical")
 })
 
-tp <- plot(xm, xs, pal = "Cividis", rev = TRUE, pages = 1)
 test_that("plot completes silently - predicted", {
+  tp <- plot(xm, xs, pal = "Cividis", rev = TRUE, pages = 1)
   expect_s3_class(tp, c("gg","ggplot"))
 })
 
